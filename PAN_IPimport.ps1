13015445d77b4bc19dd9a32bec30c147ead9f878
Ryan Brunkhorst <16407142+ryanbrunkhorst@users.noreply.github.com>
2019-01-09 12:25:42 -0800

    Add files via upload

---
+++ PAN_IPimport.ps1
@@ -0,0 +1,5 @@
+﻿$json = (Invoke-RestMethod -Method Get -Headers $headers -ContentType 'application/json' -Uri "https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7")
+$ips = ($json | select -ExpandProperty "ips" -ErrorAction Ignore)
+$urls = ($json | select -ExpandProperty "urls" -ErrorAction Ignore)
+$ips | add-content "$PSScriptRoot\o365_ips.txt"
+$urls | add-content "$PSScriptRoot\o365_urls.txt"
